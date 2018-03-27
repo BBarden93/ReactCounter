61ffafe2c228a5caadfba2cd828228a6e68c0fcc
Brittany Barden <bbarden93@yahoo.com>
2018-03-27 12:20:00 -0700

    seperate functionality to counter component

--- src/App.js
+++ src/App.js
@@ -2,28 +2,10 @@ import React from 'react'
 
 class App extends React.Component {
     
-    handlePlusClick() {
-        this.setState({
-            count: this.state.count + 1
-        })
-        console.log("Plus clicked")
-    }
-    handleMinusClick() {
-        if(this.state.count > 0) {
-            this.setState({
-                count: this.state.count - 1
-            })
-        }
-        console.log("Minus clicked")
-    }
-    
     render(){
         return (
             <div className="App">
                 <h1>Counter App</h1>
-                <h2>{this.state.count}</h2>
-                <button onClick={this.handlePlusClick.bind(this)}>+</button>
-                <button onClick={this.handleMinusClick.bind(this)}>-</button>
             </div>
         )
     }
--- src/Counter.js
+++ src/Counter.js
@@ -8,10 +8,28 @@ class Counter extends React.Component {
             count: 0
         }
     }
+    handlePlusClick() {
+        this.setState({
+            count: this.state.count + 1
+        })
+        console.log("Plus clicked")
+    }
+    handleMinusClick() {
+        if(this.state.count > 0) {
+            this.setState({
+                count: this.state.count - 1
+            })
+        }
+        console.log("Minus clicked")
+    }
 
     render() {
         return (
-
+            <div>
+                <h2>{this.state.count}</h2>
+                <button onClick={this.handlePlusClick.bind(this)}>+</button>
+                <button onClick={this.handleMinusClick.bind(this)}>-</button>
+            </div>
         )
     }
 }
